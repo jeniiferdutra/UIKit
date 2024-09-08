61e8984a9a90c0ad49fe7d72f7b2daaac86f049a
jenifer rocha <jeniferdutra2000@icloud.com>
2024-09-08 20:54:50 -0300

    configuracao inicial

---
+++ KingBurguer/KingBurguer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KingBurguer
+//
+//  Created by Jenifer Rocha on 06/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.blue
+    }
+
+
+}
+
