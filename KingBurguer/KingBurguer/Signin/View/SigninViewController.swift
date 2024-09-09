9952b554ce24aa7faaf7d79ceeaf7d5c4b631684
jenifer rocha <jeniferdutra2000@icloud.com>
2024-09-09 19:38:09 -0300

    componentes da interface

--- KingBurguer/KingBurguer/Signin/View/SigninViewController.swift
+++ KingBurguer/KingBurguer/Signin/View/SigninViewController.swift
@@ -10,15 +10,18 @@ import UIKit
 
 class SigninViewController: UIViewController {
     
-    let email: UITextField = UITextField()
+    let email: UITextField = { // centralizar os componentes num lugar só
+        let ed = UITextField()
+        ed.backgroundColor = .white
+        ed.placeholder = "Entre com seu e-mail"
+        return ed
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .green // qnd for enum podemos emitir o nome da enum
         
         view.addSubview(email) // jogar uma outra view (email), uma hierarquia
-        email.backgroundColor = .white
-        email.placeholder = "Entre com seu e-mail"
         email.frame = CGRect(x: 0, y: view.bounds.size.height / 2, width: view.bounds.size.width, height: 50)
         
     }
