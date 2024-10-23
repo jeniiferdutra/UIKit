a7d80567ae12a32ccb9500ea983f88e1835a8db4
jenifer rocha <jeniferdutra2000@icloud.com>
2024-10-23 19:34:41 -0300

    colecoes de dados com tableview

--- KingBurguer/KingBurguer/Feed/View/FeedViewViewController.swift
+++ KingBurguer/KingBurguer/Feed/View/FeedViewViewController.swift
@@ -9,16 +9,45 @@ import UIKit
 
 class FeedViewViewController: UIViewController {
     
-    let test: UIView = {
-        let v = UIView(frame: CGRect(x: 20, y: 90, width: 20, height: 20))
-        v.backgroundColor = .purple
-        return v
+    private let homeFeedTable: UITableView = {
+        let tv = UITableView()
+        // uitableview trabalha em conjunto com a uitablecell
+        tv.register(UITableViewCell.self, forCellReuseIdentifier: "cellid")
+        tv.backgroundColor = UIColor.cyan
+        return tv
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(test)
+        view.backgroundColor = .systemBackground
+        
+        view.addSubview(homeFeedTable)
+        
+        homeFeedTable.dataSource = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        homeFeedTable.frame = view.bounds
     }
 
+}
 
+extension FeedViewViewController: UITableViewDataSource {
+    
+    // numero de linhas na secao
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    // mensagem de texto
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellid", for: indexPath)
+        
+        cell.textLabel?.text = "Olá, Mundo! \(indexPath.row)" // indexpath vai mostrar a quant de linhas
+        
+        return cell
+    }
+    
+    
 }
